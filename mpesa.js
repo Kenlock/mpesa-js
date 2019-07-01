9b0d55b4179da33f35b2b3341645f347e5986dd8
Mauko Maunde <maukoese@gmail.com>
2019-07-01 23:58:35 +0300

    Create mpesa.js

---
+++ mpesa.js
@@ -0,0 +1,15 @@
+var mpesaConfig = {"headoffice": "", "shortcode": ""}
+
+var getToken = function(){
+  
+}
+
+var c2bRequest = function(){}
+
+var validateTrans = function(){}
+
+var confirmTrans = function(){}
+
+var reverseTrans = function(){}
+
+var checkBalance = function(){}
