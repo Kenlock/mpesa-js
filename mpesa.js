83abede49a32955d46f943184b92194082009aab
Mauko Maunde <maukoese@gmail.com>
2019-07-02 00:09:24 +0300

    Update mpesa.js

--- mpesa.js
+++ mpesa.js
@@ -1,15 +1,23 @@
-var mpesaConfig = {"headoffice": "", "shortcode": ""}
+var mpesaConfig = {env: "", headoffice: "", shortcode: ""}
+
+let {env, headoffice, shortcode} = mpesaConfig;
 
 var getToken = function(){
-  
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", "https://"+env+".safaricom.co.ke/", true); // true for asynchronous 
+  xmlHttp.send(null);
 }
 
-var c2bRequest = function(){}
+var c2bRequest = (phone, amount, ref) => {}
 
-var validateTrans = function(){}
+var validateTrans = (transID) => {}
 
-var confirmTrans = function(){}
+var confirmTrans = (transID = 0) => {}
 
-var reverseTrans = function(){}
+var reverseTrans = (transID = 0) => {}
 
-var checkBalance = function(){}
+var checkBalance = (transID = 0) => {}
